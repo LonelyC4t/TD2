1c2ece074b9e0060577897c83a3696de0da52b6a
Pavel Popov <pavel2003sbk@yandex.ru>
2024-05-08 00:35:33 +0800

    вынес функцию тика таймера в app.js

--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -1,67 +1,39 @@
 /* eslint-disable */
-import React from "react";
+import React from 'react';
 import './timer.css';
 
 export default class Timer extends React.Component {
+  state = {
+    sec: this.props.sec,
+    min: this.props.min,
+    clearInterval: null,
+  };
+  start = () => {
+    const timerId = setInterval(this.props.onTick, 1000);
+    this.setState({
+      clearInterval: timerId,
+    });
+  };
+  stop = () => {
+    clearInterval(this.state.clearInterval);
+  };
+  componentWillUnmount() {
+    this.stop();
+  }
+  componentDidMount() {
+    this.setState({
+      sec: this.props.sec,
+      min: this.props.min,
+    });
+  }
 
-    state = {
-        sec: 0,
-        min: 0,
-        clearInterval: null,
-        currentTime: {
-            sec: 0,
-            min: 0
-        }
-    };
-    tic = () => {
-        if(this.state.sec >= 59) {
-            this.setState((prevState) => ({
-                sec: -1,
-                min: prevState.min + 1,
-            }));
-        };
-        this.setState((prevState) => ({
-            sec: prevState.sec + 1,
-        }));
-    };
-    start = () => {
-        const timerId = setInterval(this.tic, 1000);
-        this.setState({
-            clearInterval: timerId,
-        });
-    };
-    stop = () => {
-        clearInterval(this.state.clearInterval);
-    };
-    componentWillUnmount() {
-       this.stop()
-        let second = this.state.sec;
-        let minute = this.state.min;
-        console.log(minute, second);
-        this.setState(({
-            currentTime: {
-                sec: second, 
-                min: 5, 
-            }
-        }), () => console.log(this.state.currentTime));
-
-    };
-    componentDidMount(){
-
-        this.setState({
-            sec: this.state.currentTime.sec,
-            min: this.state.currentTime.min,
-        })
-    };
-    
-    render() {
-        
-        return (
-            <span className="timer">
-              <button onClick={this.start} className="iconPlay"></button>
-              <button  onClick={this.stop} className="iconPause"></button>
-              <span className="time"> {`${this.state.min}:${this.state.sec <= 9 ? 0 : ""}${this.state.sec}`} </span>
-            </span>
-        );
-    };
-};
+  render() {
+    return (
+      <span className="timer">
+        <button onClick={this.start} className="iconPlay"></button>
+        <button onClick={this.stop} className="iconPause"></button>
+        <span className="time"> {`${this.props.min}:${this.props.sec <= 9 ? 0 : ''}${this.props.sec}`} </span>
+      </span>
+    );
+  }
+}
