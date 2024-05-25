ad2c0883f439d9c5fc7d14d31878eee5d61ce975
Evgeny <LyapinE@gmail.com>
2024-05-25 19:58:27 +0300

    fix

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import Header from '../header/header';
 import TodoBody from '../todoBody/todoBody';
